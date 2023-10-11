47937266f9f8fa7f4b028ef0fa667ae0d73c9834
Andrew Chernyshev <andrei.chernyshev93@mail.ru>
2023-10-11 16:03:44 +0300

    Задание выполнено

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,28 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+
+        userService.saveUser("Bob", "Dilan", (byte) 38);
+        System.out.println("User с именем Bob добавлен в базу данных");
+        userService.saveUser("Robert", "Lowson", (byte) 31);
+        System.out.println("User с именем Robert добавлен в базу данных");
+        userService.saveUser("Mike", "Klimson", (byte) 24);
+        System.out.println("User с именем Mike добавлен в базу данных");
+        userService.saveUser("Dan", "Balan", (byte) 42);
+        System.out.println("User с именем Dan добавлен в базу данных");
+
+        System.out.println(userService.getAllUsers());
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String HOST = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Rose1504*";
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            connection = DriverManager.getConnection(HOST, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
